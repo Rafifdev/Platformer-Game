4724551b960d2df6ac3a5104566fb179cb4524a9
Rafif <merafif28@gmail.com>
2024-12-08 12:16:46 +0700

    Menambahkan slime dan fire

--- main.py
+++ main.py
@@ -8,23 +8,104 @@ screen_height = 800
 screen_width = 800
 
 screen = pygame.display.set_mode((screen_height,screen_width))
-pygame.display.set_caption("Platformer Game by Rafif, Andi, & Bagus")
+pygame.display.set_caption("Platformer Game by Andi, Bagus & Rafif")
+
+tile_size = 40
 
 # Load image
 bg_img = pygame.image.load('img/sky.jpg')
 sun_img = pygame.image.load('img/sun.png')
 
-# variabel game
-tile_size = 160
+def white_grid():
+  for line in range(0, 21):
+    pygame.draw.line(screen, (255,255,255), (0, line * tile_size), (screen_width, line * tile_size))
+    pygame.draw.line(screen, (255,255,255), (line * tile_size, 0), (line * tile_size, screen_height))
+    
+class World():
+  def __init__(self, data):
+    self.tile_list = []
+    dirt_img = pygame.image.load('img/dirt.png')
+    grass_img = pygame.image.load('img/grass.png')
+    fire_img = pygame.image.load('img/fire.png')
+    slime_img = pygame.image.load('img/slime.png')
+    
+    row_count = 0
+    for row in data:
+      col_count = 0
+      for tile in row:
+        if tile == 1:
+          img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
+          img_rect = img.get_rect()
+          img_rect.x = col_count * tile_size
+          img_rect.y = row_count * tile_size
+          tile = (img, img_rect)
+          self.tile_list.append(tile)
+        if tile == 2:
+          img = pygame.transform.scale(grass_img, (tile_size, tile_size))
+          img_rect = img.get_rect()
+          img_rect.x = col_count * tile_size
+          img_rect.y = row_count * tile_size
+          tile = (img, img_rect)
+          self.tile_list.append(tile)
+        if tile == 3:
+          img = pygame.transform.scale(slime_img,(tile_size, tile_size))
+          img_rect = img.get_rect()
+          img_rect.x = col_count * tile_size
+          img_rect.y = row_count * tile_size
+          tile = (img, img_rect)
+          self.tile_list.append(tile)
+        if tile == 6:
+          img = pygame.transform.scale(fire_img, (tile_size, tile_size))
+          img_rect = img.get_rect()
+          img_rect.x = col_count * tile_size
+          img_rect.y = row_count * tile_size
+          tile = (img, img_rect)
+          self.tile_list.append(tile)
+        col_count += 1
+      row_count += 1
+    
+  def draw(self):
+    for tile in self.tile_list:
+        screen.blit(tile[0], tile[1])
 
-running = True
-while running:
-  screen.blit(bg_img, (0,0))
-  screen.blit(sun_img, (350,60))
+world_map = [
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1], 
+[1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 2, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 7, 0, 5, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 1], 
+[1, 7, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1], 
+[1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 7, 0, 0, 0, 0, 2, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 2, 2, 2, 2, 1], 
+[1, 0, 0, 0, 0, 0, 2, 2, 2, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
 
+world = World(world_map)
+
+# Looping untuk menampilkan
+start = True
+while start:
+  screen.blit(bg_img, (0, 0))
+  screen.blit(sun_img, (350, 60))
+  
+  world.draw()
+  # white_grid()
+
+# Looping untuk mengambil koordinat
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
-        running = False
+        start = False
 
   pygame.display.update()
 pygame.quit()
