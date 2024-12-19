bbbda6b12871ccaffc026aeca030e1c9ce7ddd3b
Rafif <merafif28@gmail.com>
2024-12-19 07:46:58 +0700

    Menambah sistem control

---
+++ main-copy.py
@@ -0,0 +1,63 @@
+import pygame
+
+# Inisialisasi modul Pygame
+pygame.init()
+
+# Mengatur ukuran layar (width x height) dalam piksel
+screen_height = 800
+screen_width = 800
+
+# Membuat layar game dengan ukuran yang telah ditentukan
+screen = pygame.display.set_mode((screen_height,screen_width))
+
+# Memberikan judul pada game
+pygame.display.set_caption("Platformer Game by Andi, Bagus & Rafif")
+
+# Ukuran setiap tile (kotak) dalam game
+tile_size = 40
+
+# Memuat gambar latar belakang dan matahahari
+bg_img = pygame.image.load('img/sky.jpg') 
+sun_img = pygame.image.load('img/sun.png')
+
+world_map = [
+  # 1 = tanah, 2 = rumput, 3 = slime, 6 = api, 0 = kosong
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+  [1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1], 
+  [1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 2, 1], 
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 7, 0, 5, 0, 0, 0, 1], 
+  [1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 1], 
+  [1, 7, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+  [1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1], 
+  [1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+  [1, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 1], 
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1], 
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 7, 0, 0, 0, 0, 2, 0, 1], 
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 2, 2, 2, 2, 1], 
+  [1, 0, 0, 0, 0, 0, 2, 2, 2, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1], 
+  [1, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+  [1, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+  [1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+# Loop utama untuk menjalankan game
+start = True
+while start:
+  # Menggambar latar belakang dan matahari
+  screen.blit(bg_img, (0, 0))  # Menggambar latar belakang di koordinat (0, 0)
+  screen.blit(sun_img, (350, 60))  # Menggambar matahari di koordinat (350, 60)
+
+  # untuk keluar dari game
+  for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+          start = False
+
+  # Memperbarui tampilan layar
+  pygame.display.update()
+
+# Keluar dari Pygame setelah game ditutup
+pygame.quit()
--- main.py
+++ main.py
@@ -1,38 +1,88 @@
 import pygame
-
-# Installasi
 pygame.init()
 
-# Screen size
 screen_height = 800
 screen_width = 800
+tile_size = 40
 
 screen = pygame.display.set_mode((screen_height,screen_width))
 pygame.display.set_caption("Platformer Game by Andi, Bagus & Rafif")
-
-tile_size = 40
-
-# Load image
-bg_img = pygame.image.load('img/sky.jpg')
+bg_img = pygame.image.load('img/sky.jpg') 
 sun_img = pygame.image.load('img/sun.png')
 
 def white_grid():
   for line in range(0, 21):
     pygame.draw.line(screen, (255,255,255), (0, line * tile_size), (screen_width, line * tile_size))
     pygame.draw.line(screen, (255,255,255), (line * tile_size, 0), (line * tile_size, screen_height))
+
+class Player():
+  def __init__(self, x, y):
+    img = pygame.image.load('img/player1.png')
+    self.image = pygame.transform.scale(img, (40, 80))
+    self.rect = self.image.get_rect()
+    self.rect.x = x
+    self.rect.y = y
+    self.graf_y = 0
+    self.jumped = False
     
+  def update(self):
+    mx = 0
+    my = 0
+    
+    key = pygame.key.get_pressed()
+    if key[pygame.K_SPACE] and self.jumped == False:
+      self.graf_y = -5
+      self.jumped = True
+    if key[pygame.K_SPACE] == False:
+      self.jumped = False
+    if key[pygame.K_LEFT]:
+      mx -= 0.8
+    if key[pygame.K_RIGHT]:
+      mx += 0.8
+      
+    if key[pygame.K_1]:
+      my -= 1.5
+    if key[pygame.K_2]:
+      my += 1.5
+      
+    # Gravitasi
+    self.graf_y += 0.1
+    if self.graf_y >= 1:
+      self.graf_y = 0.1
+    my += self.graf_y
+    
+    self.rect.x += mx
+    self.rect.y += my
+
+    if self.rect.bottom > screen_height:
+      self.rect.bottom = screen_height
+    
+    if self.rect.right > screen_width:
+        self.rect.right = screen_width
+
+    if self.rect.left < 0:
+        self.rect.left = 0
+        
+    if self.rect.top < 0:
+        self.rect.top = 0
+
+    
+    screen.blit(self.image, self.rect)
+
 class World():
   def __init__(self, data):
-    self.tile_list = []
+    self.tile_list = [] 
+    
     dirt_img = pygame.image.load('img/dirt.png')
     grass_img = pygame.image.load('img/grass.png')
     fire_img = pygame.image.load('img/fire.png')
     slime_img = pygame.image.load('img/slime.png')
     
-    row_count = 0
+    row_count = 0 
     for row in data:
-      col_count = 0
+      col_count = 0 
       for tile in row:
+        
         if tile == 1:
           img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
           img_rect = img.get_rect()
@@ -40,6 +90,7 @@ class World():
           img_rect.y = row_count * tile_size
           tile = (img, img_rect)
           self.tile_list.append(tile)
+          
         if tile == 2:
           img = pygame.transform.scale(grass_img, (tile_size, tile_size))
           img_rect = img.get_rect()
@@ -47,6 +98,7 @@ class World():
           img_rect.y = row_count * tile_size
           tile = (img, img_rect)
           self.tile_list.append(tile)
+          
         if tile == 3:
           img = pygame.transform.scale(slime_img,(tile_size, tile_size))
           img_rect = img.get_rect()
@@ -54,6 +106,7 @@ class World():
           img_rect.y = row_count * tile_size
           tile = (img, img_rect)
           self.tile_list.append(tile)
+        
         if tile == 6:
           img = pygame.transform.scale(fire_img, (tile_size, tile_size))
           img_rect = img.get_rect()
@@ -61,51 +114,51 @@ class World():
           img_rect.y = row_count * tile_size
           tile = (img, img_rect)
           self.tile_list.append(tile)
-        col_count += 1
-      row_count += 1
-    
+        col_count += 1 
+      row_count += 1 
+
   def draw(self):
     for tile in self.tile_list:
         screen.blit(tile[0], tile[1])
 
 world_map = [
-[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
-[1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1], 
-[1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 2, 1], 
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 7, 0, 5, 0, 0, 0, 1], 
-[1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 1], 
-[1, 7, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
-[1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1], 
-[1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
-[1, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 1], 
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1], 
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 7, 0, 0, 0, 0, 2, 0, 1], 
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 2, 2, 2, 2, 1], 
-[1, 0, 0, 0, 0, 0, 2, 2, 2, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1], 
-[1, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
-[1, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
-[1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+  [1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1], 
+  [1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 2, 1], 
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 7, 0, 5, 0, 0, 0, 1], 
+  [1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 1], 
+  [1, 7, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+  [1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1], 
+  [1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+  [1, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 1], 
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1], 
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 7, 0, 0, 0, 0, 2, 0, 1], 
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 2, 2, 2, 2, 1], 
+  [1, 0, 0, 0, 0, 0, 2, 2, 2, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1], 
+  [1, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+  [1, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+  [1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
 world = World(world_map)
+player = Player(80, 680)
 
-# Looping untuk menampilkan
 start = True
 while start:
-  screen.blit(bg_img, (0, 0))
+  screen.blit(bg_img, (0, 0)) 
   screen.blit(sun_img, (350, 60))
-  
+
   world.draw()
-  # white_grid()
+  player.update()
+  # white_grid() 
 
-# Looping untuk mengambil koordinat
   for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-        start = False
-
+      if event.type == pygame.QUIT:
+          start = False
+          
   pygame.display.update()
 pygame.quit()
